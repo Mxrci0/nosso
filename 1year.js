6036c7e6c867e061fb2b798f5c001b96f1c9b932
Mxrci0 <162761391+Mxrci0@users.noreply.github.com>
2025-04-02 20:07:44 -0300

    Add files via upload

---
+++ 1year.js
@@ -0,0 +1,15 @@
+function updateCounter() {
+    const startDate = new Date("2024-04-22T00:00:00");
+    const now = new Date();
+    const timeDiff = now - startDate;
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    document.getElementById("counter").innerText = `${days}d ${hours}h ${minutes}m ${seconds}s desde 22/04/2024`;
+}
+
+setInterval(updateCounter, 1000);
+updateCounter();
